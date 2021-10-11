e1c8c3ea6ec5ec1144e9f1c7832180e39d4719b3
ircg2011 <67656558+ircg2011@users.noreply.github.com>
2021-10-11 16:09:11 -0300

    Final code
    Upload final code

--- Section4/app.js
+++ Section4/app.js
@@ -1,6 +1,33 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 
-console.clear();
-const base = 5;
-for (let i = 1; i<=10; i++){
-  console.log(`${base} x ${i} = ${base*i}` );
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+
 }
+
+
+
+// console.log(argv.base);
+
+
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
